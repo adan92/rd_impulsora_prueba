adbf89bfa56f1b4c32efda11529ada53a1f073ba
Christian Adan Israel Amezcua Aguilar <amezcua9205@gmail.com>
2025-03-03 21:22:37 -0600

    Se ajusto el urls.py general y en el .sh

--- confi/urls.py
+++ confi/urls.py
@@ -14,20 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-from rest_framework.permissions import AllowAny
-from rest_framework.schemas import get_schema_view
-from drf_yasg import openapi
-"""
-schema_view = get_schema_view(
-    openapi.Info(title="Gastos Api", default_version='v1'),
-    public=True,
-    permission_classes=(AllowAny,),
-)
-"""
 
 urlpatterns = [
     path('api/', include('api.urls')),
-  #  path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
